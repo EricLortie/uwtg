28193792b635146466164c2180f32b7f80710b42
Eric Lortie <e@ericlortie.com>
2017-02-14 16:09:29 -0400

    Bunch of work on character pages.

--- archive-characters.php
+++ archive-characters.php
@@ -9,47 +9,79 @@
 <?php get_header(); ?>
 <div class="container">
 	<div class="row">
-		<div class="col-sm-7">
+		<div class="col-sm-12">
 			<section id="blog">
 				<h1>Characters</h1>
+				<p>Here you'll find a list of all currently listed Tempest Grove characters.
+					This list includes <strong>Player Characters</strong> and <strong>Non-Player Characters</strong>.<p>
+				<p>If you'd like to see your character listed with a detailed biography please <a href="/new-character-form" target="_blank">fill out this form.</a></p>
+				<br/>
+				<hr/>
+				<br/>
 				<?php do_action( 'mtl_above_content_after_header' ); ?>
 				<?php
-				if( have_posts() ):
-					while( have_posts() ):
-						the_post();
-                        $character_entry['race'] = uit_get_post_custom_value('race');
-                        $character_entry['class'] = uit_get_post_custom_value('class');
-                        ?>
 
-												<hr />
-                        <article id="post-<?php the_ID(); ?>" <?php post_class( 'blog-post archive-post' ); ?>>
-                            <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" class="blog-post-title"><?php the_title(); ?></a>
-                            <div class="row">
-                                <div class="col-sm-4">
-                                    <?php if( has_post_thumbnail() ): ?>
-                                        <div class="blog-post-image character-photo">
-                                            <?php the_post_thumbnail( 'illdy-blog-list' ); ?>
-                                        </div><!--/.blog-post-image-->
-                                    <?php endif; ?>
-                                </div>
-                                <div class="col-sm-8">
-                                    <div class="testimonial-meta" style="font-style: italic; font-weight:bolder;font-size: 200%;">
-                                        <?php echo $character_entry['race'] ?> <?php echo $character_entry['class'] ?>
-                                    </div><!--/.testimonial-meta-->
-                                    <?php the_excerpt(); ?>
-                                    <a href="<?php the_permalink(); ?>" title="<?php _e( 'Read more', 'illdy' ); ?>" class="blog-post-button"><?php _e( 'Read more', 'illdy' ); ?></a>
-                                </div>
-                            </div>
-                        </article>
-                    <?php endwhile;
-				else:
-					get_template_part( 'template-parts/content', 'none' );
-				endif;
-				?>
+				// Args
+				$args = array(
+						'post_type'         => 'characters',
+						'posts_per_page'         => -1,
+						'orderby'						=> 'title',
+						'order' 						=> 'ASC'
+
+				);
+
+				// The Query
+				$query1 = new WP_Query($args); ?>
+
+				<div class="row">
+
+					<?php // The Loop
+					$i = 0;
+					while ($query1->have_posts()) {
+						$query1->the_post();
+	          $character_entry['race'] = uit_get_post_custom_value('race');
+	          $character_entry['class'] = uit_get_post_custom_value('class');
+						$content = get_the_content();
+						$excerpt = get_the_excerpt();
+						$show_content = true;
+						if($content == $excerpt){
+							$show_content = false;
+						}
+	          ?>
+						<div class="col-sm-6">
+		          <article id="post-<?php the_ID(); ?>" <?php post_class( 'blog-post archive-post' ); ?>>
+		              <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" class="blog-post-title"><?php the_title(); ?></a>
+		              <div class="row">
+		                  <div class="col-sm-4">
+		                      <?php if( has_post_thumbnail() ): ?>
+		                          <div class="blog-post-image character-photo">
+		                              <?php the_post_thumbnail( 'illdy-blog-list' ); ?>
+		                          </div><!--/.blog-post-image-->
+		                      <?php endif; ?>
+		                  </div>
+		                  <div class="col-sm-8">
+		                      <div class="testimonial-meta" style="font-style: italic; font-weight:bolder;font-size: 200%;">
+		                          <?php echo $character_entry['race'] ?> <?php echo $character_entry['class'] ?>
+		                      </div><!--/.testimonial-meta-->
+		                      <?php the_excerpt(); ?>
+													<?php if ($show_content){ ?>
+		                      	<a href="<?php the_permalink(); ?>" title="<?php _e( 'Read more', 'illdy' ); ?>" class="blog-post-button"><?php _e( 'Read more', 'illdy' ); ?></a>
+													<?php } ?>
+											</div>
+		              </div>
+		          </article>
+						</div>
+						<?php
+		        $i++;
+		        if ($i % 2 == 0) {
+		            echo "</div><div class='row'>";
+		        }
+						?>
+		       <?php } ?>
+				 </div>
 				<?php do_action( 'mtl_after_content_above_footer' ); ?>
 			</section><!--/#blog-->
 		</div><!--/.col-sm-7-->
-		<?php get_sidebar(); ?>
 	</div><!--/.row-->
 </div><!--/.container-->
 <?php get_footer(); ?>
--- single-characters.php
+++ single-characters.php
@@ -16,48 +16,51 @@
 					while( have_posts() ):
 						the_post();
 
-                        $character_entry['race'] = uit_get_post_custom_value('race');
-                        $character_entry['class'] = uit_get_post_custom_value('class');
-												$content = get_the_content();
-												$excerpt = get_the_excerpt();
-												$show_content = true;
-												if($content == $excerpt){
-													$show_content = false;
-												}
-                        ?>
+	          $character_entry['race'] = uit_get_post_custom_value('race');
+	          $character_entry['class'] = uit_get_post_custom_value('class');
+						$content = get_the_content();
+						$excerpt = get_the_excerpt();
+						$show_content = true;
+						if($content == $excerpt){
+							$show_content = false;
+						}
+	          ?>
 
-                        <article id="post-<?php the_ID(); ?>" <?php post_class( 'blog-post' ); ?>>
-                            <div class="row">
-                                <div class="col-sm-6">
-                                    <?php if( has_post_thumbnail() ): ?>
-                                        <div class="blog-post-image character-photo">
-                                            <?php the_post_thumbnail( 'illdy-blog-list' ); ?>
-                                        </div><!--/.blog-post-image-->
-                                    <?php endif; ?>
-                                </div>
-                                <div class="col-sm-6">
-                                    <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" class="blog-post-title"><?php the_title(); ?></a>
-                                    <div class="testimonial-meta character-meta" style="font-style: italic; font-weight:normal;">
-                                        <?php echo $character_entry['race'] ?> <?php echo $character_entry['class'] ?>
-                                    </div><!--/.testimonial-meta-->
-				                            <div class="blog-post-excerpt">
-				                                <?php echo $excerpt; ?>
-				                            </div><!--/.blog-post-entry-->
+	          <article id="post-<?php the_ID(); ?>" <?php post_class( 'blog-post' ); ?>>
+	              <div class="row">
+	                  <div class="col-sm-6">
+	                      <?php if( has_post_thumbnail() ): ?>
+	                          <div class="blog-post-image character-photo">
+	                              <?php the_post_thumbnail( 'illdy-blog-list' ); ?>
+	                          </div><!--/.blog-post-image-->
+	                      <?php endif; ?>
+	                  </div>
+	                  <div class="col-sm-6">
+	                      <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" class="blog-post-title"><?php the_title(); ?></a>
+	                      <div class="testimonial-meta character-meta" style="font-style: italic; font-weight:normal;">
+	                          <?php echo $character_entry['race'] ?> <?php echo $character_entry['class'] ?>
+	                      </div><!--/.testimonial-meta-->
+	                      <div class="blog-post-excerpt">
+	                          <?php echo $excerpt; ?>
+	                      </div><!--/.blog-post-entry-->
 
-                                </div>
-                            </div>
-                            <div class="blog-post-entry">
-                                <?php
-																if($show_content){
-																	the_content();
-																}
-																?>
-                            </div><!--/.blog-post-entry-->
-                        </article><!--/#post-<?php the_ID(); ?>.blog-post-->
+	                  </div>
+	              </div>
+	              <div class="blog-post-entry">
+	                  <?php
+										if($show_content){
+											the_content();
+										}
+										?>
+	              </div><!--/.blog-post-entry-->
+	          </article><!--/#post-<?php the_ID(); ?>.blog-post-->
 
 					<?php endwhile;
 				endif;
 				?>
+				<div class="blog-post">
+					<a href="/characters" title="Back to Characters" class="blog-post-button"><?php _e( 'Back to Characters', 'illdy' ); ?></a>
+				</div>
 			</section><!--/#blog-->
 		</div><!--/.col-sm-7-->
 		<?php get_sidebar(); ?>
