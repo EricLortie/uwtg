1426eb9bd36266b95be8d9951d2160e2b6439731
Eric Lortie <e@ericlortie.com>
2017-07-20 13:53:12 -0300

    Trying to load this differently.

--- layout/js/character-builder.js
+++ layout/js/character-builder.js
@@ -1251,6 +1251,15 @@ jQuery(document).on('ready', function(){
     push_armour_to_remote(builder_data, slot, level, ap, penalty, type);
 
   });
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
   window.fbAsyncInit = function() {
 
     FB.init({
